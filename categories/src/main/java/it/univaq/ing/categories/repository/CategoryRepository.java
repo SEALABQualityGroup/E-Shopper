f37183f5fa05174a4c339019aada95de9b765f24
lucatraini <lucatra89@gmail.com>
2018-04-11 10:24:39 +0200

    Added NewSpan annotation to CategoryRepository

--- categories/src/main/java/it/univaq/ing/categories/repository/CategoryRepository.java
+++ categories/src/main/java/it/univaq/ing/categories/repository/CategoryRepository.java
@@ -2,6 +2,7 @@ package it.univaq.ing.categories.repository;
 
 import java.util.List;
 
+import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.data.repository.Repository;
 
 import it.univaq.ing.categories.domain.Category;
@@ -11,7 +12,7 @@ import it.univaq.ing.categories.domain.Category;
  * @author LC
  */
 public interface CategoryRepository extends Repository<Category, Long>{
-
+	@NewSpan
 	public List<Category> findAll();
 	
 }
