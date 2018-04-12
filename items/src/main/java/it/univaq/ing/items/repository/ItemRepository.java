975a9cc2f02fd32d8f31c4e557a8f93b298581ca
lucatraini <lucatra89@gmail.com>
2018-04-12 15:38:19 +0200

    Added NewSpan annotation to ItemRepository and ProductRepository

--- items/src/main/java/it/univaq/ing/items/repository/ItemRepository.java
+++ items/src/main/java/it/univaq/ing/items/repository/ItemRepository.java
@@ -3,6 +3,7 @@ package it.univaq.ing.items.repository;
 
 import java.util.List;
 
+import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
@@ -17,30 +18,39 @@ import it.univaq.ing.items.domain.Item;
 public interface ItemRepository extends Repository<Item, Long>{
 	
 	
+	@NewSpan
 	@Query(value = "SELECT * FROM ITEM WHERE PRODUCT_ID = ?1", nativeQuery = true)
 	public List<Item> getItemsByIdProduct(Long idProduct);	
 
+	@NewSpan
 	@Query(value = "SELECT * FROM ITEM WHERE CATEGORY_ID = ?1", nativeQuery = true)
 	public List<Item> getItemsByIdCategory(Long idCategory);
 
+	@NewSpan
 	@Query("FROM Item WHERE itemId = :idItem")
 	public Item getItemById(@Param("idItem") Long idItem);
 	
+	@NewSpan
 	@Query(value = "SELECT * FROM ITEM WHERE CATEGORY_ID = ?1  AND UNIT_COST BETWEEN ?2 AND ?3", nativeQuery = true)
 	public List<Item> findItemByCategoryFilter(Long idCategory, Long priceMin, Long priceMax);
 	
+	@NewSpan
 	@Query(value = "SELECT * FROM ITEM WHERE PRODUCT_ID = ?1  AND UNIT_COST BETWEEN  ?2 AND ?3", nativeQuery = true)
 	public List<Item> findItemByProductFilter(Long idProduct, Long priceMin, Long priceMax);
 	
+	@NewSpan
 	@Query(value = "SELECT * FROM ITEM WHERE PRODUCT_ID = ?1 ORDER BY RAND() LIMIT 0,3", nativeQuery = true)
 	public List<Item> findItemsRandomByIdProduct(Long idProduct);
 	
+	@NewSpan
 	@Query(value = "SELECT * FROM ITEM  ORDER BY RAND() LIMIT 0,3", nativeQuery = true)
 	public List<Item> findItemRandom();
 	
+	@NewSpan
 	@Query(value = "SELECT * FROM ITEM WHERE TOT_QUANTITY < 4  ORDER BY RAND() LIMIT 0,3", nativeQuery = true)
 	public List<Item> findFeaturesItemRandom();
 	
+	@NewSpan
 	public Item save(Item account);
 		
 }
--- products/src/main/java/it/univaq/ing/products/repository/ProductRepository.java
+++ products/src/main/java/it/univaq/ing/products/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package it.univaq.ing.products.repository;
 
 import java.util.List;
 
+import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
@@ -13,16 +14,20 @@ import it.univaq.ing.products.domain.Product;
  *
  */
 public interface ProductRepository extends Repository<Product, Long>{
-
+	
+	@NewSpan
 	@Query("FROM Product")
 	public List<Product> findProducts();
-
+	
+	@NewSpan
 	@Query("FROM Product WHERE category_id= :categoryId")
 	public List<Product> findProductByCategory(@Param("categoryId") Long categoryId);
 	
+	@NewSpan
 	@Query("FROM Product WHERE product_id= :productId")
 	public Product findProductById(@Param("productId") Long productId);
 	
+	@NewSpan
 	@Query(value = "SELECT * FROM PRODUCT  ORDER BY RAND() LIMIT 0,5", nativeQuery = true)
 	public List<Product> findProductsRandom();
 	
