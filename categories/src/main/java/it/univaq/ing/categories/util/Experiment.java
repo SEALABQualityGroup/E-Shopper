74d792bbf69b639a3ce476975c48cc233e012fff
lucatraini <lucatra89@gmail.com>
2019-09-13 10:13:01 +0200

    Fixed unsafe experiment

--- categories/src/main/java/it/univaq/ing/categories/util/Experiment.java
+++ categories/src/main/java/it/univaq/ing/categories/util/Experiment.java
@@ -8,15 +8,22 @@ import org.springframework.cloud.sleuth.Span;
 public class Experiment{
 	protected Logger logger = Logger.getLogger(Experiment.class.getName());
 
-	public static void injectLatency(Span span, List<String> latencyInjections){
-        int requestClass = Integer.valueOf(span.getBaggageItem("experiment"));
-        int latency  = Integer.parseInt(latencyInjections.get(requestClass));
-        if (latency > 0) {
+	private static void doInjection(int requestClass, List<String> latencyInjections){
+		int latency  = Integer.parseInt(latencyInjections.get(requestClass));
+		if (latency > 0) {
 			try {
 				Thread.sleep(latency);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+		}
+}
+
+	public static void injectLatency(Span span, List<String> latencyInjections){
+		String experiment = span.getBaggageItem("experiment");
+		if (experiment != null){
+			int requestClass = Integer.valueOf(experiment);
+			doInjection(requestClass, latencyInjections);
 		}
 	}
 
